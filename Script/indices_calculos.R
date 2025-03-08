6256f12b089dafbd1a15da499d3bd0bab5213d17
dvdz03 <dflores04@alumnos.uaq.mx>
2025-03-08 01:15:42 -0600

    índice de shannon

--- Script/indices_calculos.R
+++ Script/indices_calculos.R
@@ -1,6 +1,10 @@
 ###índices de diversidad 
 
-
+shanon<-function(abundancias){
+  proba<-abundancias/sum(abundancias)
+  return(-sum(proba*log(proba)))
+}
+#según yo ese ya está bien, volví a tratar con uno de los intentos que hicimos y si salió un número más bajo, pero pueden probarlo xfa
 
 
 ##Ejercicio Luna
