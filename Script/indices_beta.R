8781d822a874e44a7330cf65cd11a92e028fbb29
Daniela Villanueva <dvillanueva16@alumnos.uaq.mx>
2025-03-09 17:00:02 -0600

    maybe

--- Script/indices_beta.R
+++ Script/indices_beta.R
@@ -19,3 +19,27 @@ jaccard(a,b)
 jaccard(a,c)
 jaccard(b,c)
 jaccard(c,d)
+
+
+###corregir
+# Función corregida para calcular Jaccard
+jaccard <- function(a, b) {
+  interseccion = length(intersect(a, b))
+  union = length(unique(c(a, b)))  # Se usa unique para evitar duplicados
+  return(interseccion / union)
+}
+# Cargar datos
+presencia <- read.csv("01_datos_crudos/datosjaccard.csv", row.names = 1)
+
+# Convertir datos en listas de especies presentes por sitio
+sitio1 <- rownames(presencia)[which(presencia$sitio1 == 1)]
+sitio2 <- rownames(presencia)[which(presencia$sitio2 == 1)]
+sitio3 <- rownames(presencia)[which(presencia$sitio3 == 1)]
+sitio4 <- rownames(presencia)[which(presencia$sitio4 == 1)]
+
+# Calcular índices de Jaccard correctamente
+jaccard(sitio1, sitio2)  
+jaccard(sitio1, sitio3)  
+jaccard(sitio2, sitio3)  
+jaccard(sitio3, sitio4)  
+
