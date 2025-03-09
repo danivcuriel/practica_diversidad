0a5e69f5d4ed4488d8e22ca230118b237443ccec
Daniela Villanueva <dvillanueva16@alumnos.uaq.mx>
2025-03-09 13:52:41 -0600

    cambios

--- Script/indices_calculos.R
+++ Script/indices_calculos.R
@@ -1,6 +1,25 @@
-###índices de diversidad 
+###Índices de diversidad 
+mi_shannon <- function (abundances) {
+  prob <- abundances/sum(abundances)  ###que tan equitatitvas estan distribuidas las edades 
+  return(-sum(prob+log(prob)))
+}
+
+mi_pielou <- function (abundances){
+  mi_shannon(abundances)/log(length(abundances))
+}
+
+
+mi_simpson <- function (abundances) {
+  prob <- abundances/sum(abundances)
+  return(sum(prob^2)) ##probabilidades de que dos individuos tomados de la misma especie 
+}
+
 
 
 
 
 ##Ejercicio Luna
+
+
+
+
