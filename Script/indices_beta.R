489dc8ba0942f4b40a120c0125cf963b92c79d77
dvdz03 <dflores04@alumnos.uaq.mx>
2025-03-10 01:16:02 -0600

    matrices

--- Script/indices_beta.R
+++ Script/indices_beta.R
@@ -1,5 +1,6 @@
 #DIVERSIDAD BETA
-
+install.packages("vegan")
+library(vegan)
 #ÍNDICE JACCARD
 jaccard<-function(a,b){
   interseccion=length(which(a==1 & b==1))
@@ -49,6 +50,12 @@ jaccard(f,g)
 #es decir, no se hizo jaccard(b,a) porque da lo mismo que jaccard(a,b)
 #es lo mismo que los demás
 
+#MATRIZ DE JACCARD
+abund1<-presencia[,-1]
+abund1
+jaccardmat<-vegdist(abund1, method="jaccard")
+jaccardmat
+
 
 #ÍNDICE BRAY-CURTIS
 braycurtis<-function(a,b){
@@ -58,6 +65,45 @@ braycurtis<-function(a,b){
   return(1-arri/aba)
 }
 
+#DATOS PARA BRAY-CURTIS
+datobray<-read.csv("01_datos_crudos/datosbray.csv")
+datobray
+#PARA SITIO 1
+braycurtis(datobray$sitio1,datobray$sitio2)
+braycurtis(datobray$sitio1,datobray$sitio3)
+braycurtis(datobray$sitio1,datobray$sitio4)
+braycurtis(datobray$sitio1,datobray$sitio5)
+braycurtis(datobray$sitio1,datobray$sitio6)
+braycurtis(datobray$sitio1,datobray$sitio7)
+#PARA SITIO 2
+braycurtis(datobray$sitio2,datobray$sitio2)
+braycurtis(datobray$sitio2,datobray$sitio3)
+braycurtis(datobray$sitio2,datobray$sitio4)
+braycurtis(datobray$sitio2,datobray$sitio5)
+braycurtis(datobray$sitio2,datobray$sitio6)
+braycurtis(datobray$sitio2,datobray$sitio7)
+#PARA SITIO 3
+braycurtis(datobray$sitio3,datobray$sitio3)
+braycurtis(datobray$sitio3,datobray$sitio4)
+braycurtis(datobray$sitio3,datobray$sitio5)
+braycurtis(datobray$sitio3,datobray$sitio6)
+braycurtis(datobray$sitio3,datobray$sitio7)
+#PARA SITIO 4
+braycurtis(datobray$sitio4,datobray$sitio4)
+braycurtis(datobray$sitio4,datobray$sitio5)
+braycurtis(datobray$sitio4,datobray$sitio6)
+braycurtis(datobray$sitio4,datobray$sitio7)
+#PARA SITIO 5
+braycurtis(datobray$sitio5,datobray$sitio5)
+braycurtis(datobray$sitio5,datobray$sitio6)
+braycurtis(datobray$sitio5,datobray$sitio7)
+#PARA SITIO 6
+braycurtis(datobray$sitio6,datobray$sitio6)
+braycurtis(datobray$sitio6,datobray$sitio7)
 
-
+#MATRIZ DE BRAY
+abund<-datobray[,-1]
+abund
+braycurtismat<-vegdist(abund, method="bray")
+print(braycurtismat)
 
